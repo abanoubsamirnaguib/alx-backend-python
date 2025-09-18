bfdfccdda7ed09f1270e09a07893cc388728f17b
abanoubsamirnaguib <abanoubsamir@gmail.com>
2025-09-18 18:44:58 +0300

    Add unit tests for access_nested_map function, including exception handling

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that access_nested_map returns the expected result."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
+    
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test case foer access_nested_map function exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
 
 if __name__ == "__main__":
     unittest.main()
